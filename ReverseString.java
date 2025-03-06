8463384f640fbd32e3f7037c916238237f4668af
Yagyata <yagyatasharmaa@gmail.com>
2025-03-06 15:17:13 +0530

    [Yagyata] ADD: Reverse a string wihout using built-in reverse functions

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Reverse the string
+        String reversed = reverseString(input);
+
+        // Display the reversed string
+        System.out.println("Reversed string: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        char[] chars = str.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            // Swap characters
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+}
