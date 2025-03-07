8ab7aed7f58d6b497081fc497ec454ce390f5b68
Yagyata <yagyatasharmaa@gmail.com>
2025-03-07 13:24:56 +0530

    [Yagyata] ADD: Check if the two strings are anagrams of each other

---
+++ AnagramChecker.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        // Checking if the strings are anagrams
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+
+    // Method to check if two strings are anagrams
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths are different, they can't be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to character arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare sorted arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
+
+//Input;
+//Enter first string: abbcd
+//Enter second string: dcabb
+
+//Output;
+//The strings are anagrams.
